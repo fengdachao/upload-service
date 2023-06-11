f22f7f43acb814ea9e436fee2182d9638d5f6751
fengdachao <fengdachao@aliyun.com>
2023-06-11 12:58:55 +0800

    cors

--- image-server.js
+++ image-server.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const cors = require('cors')
 
 const app = express()
 app.listen(8080, () => {
   console.log("image server listen 8080.")
 })
+app.use(cors())
 app.use(express.static("./images"))
